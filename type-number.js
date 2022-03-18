8137e45e3d0f17543ba95e47335a2a452a7d56ef
Gisele <gisele@infolink.com.br>
2022-03-18 08:11:31 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,22 @@
+//tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSeZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+const alura = "Alura";
+console.li
